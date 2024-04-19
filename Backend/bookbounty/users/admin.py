167c48f927b72b364337504b328f1732579bb810
upmikodev <michaelgraham176@outlook.com>
2024-04-19 13:15:49 +0545

    Configured admin panel

--- Backend/bookbounty/users/admin.py
+++ Backend/bookbounty/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Book, Transaction, Category, User
+
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Book)
+admin.site.register(Transaction)
+admin.site.register(Category)
--- Backend/bookbounty/users/models.py
+++ Backend/bookbounty/users/models.py
@@ -5,9 +5,46 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
-    username = None
 
-    USERNAME_FIELD ='email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['name']
 
+    def __str__(self):
+        return self.email
+
+class Book(models.Model):
+    book_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    condition_choices = [
+        ('NEW', 'New'),
+        ('LIKE_NEW', 'Like New'),
+        ('GOOD', 'Good'),
+        ('FAIR', 'Fair'),
+        ('POOR', 'Poor'),
+    ]
+    condition = models.CharField(max_length=8, choices=condition_choices)
+    rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
+    ISBN = models.CharField(max_length=13, null=True, blank=True)
+    publication_date = models.DateField(null=True, blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    category_id = models.IntegerField(null=True, blank=True)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return self.title
+
+class Transaction(models.Model):
+    transaction_id = models.AutoField(primary_key=True)
+    date = models.DateField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sales')
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='purchases')
+
+class Category(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
