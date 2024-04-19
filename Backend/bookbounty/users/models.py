d7ca0fc66d9409b764ec99651b73a9721a5f3294
upmikodev <michaelgraham176@outlook.com>
2024-04-19 21:04:48 +0545

    Added transaction,listings,makeorder API

--- Backend/bookbounty/users/models.py
+++ Backend/bookbounty/users/models.py
@@ -32,7 +32,18 @@ class Book(models.Model):
     publication_date = models.DateField(null=True, blank=True)
     category = models.CharField(max_length=255, null=True, blank=True)
     seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='books',db_column='seller_id')
+
+    # Your existing fields...
+    status_choices = [
+        ('available', 'Available'),
+        ('order received', 'Order Received'),
+        ('sold', 'Sold'),
+    ]
+    status = models.CharField(max_length=20, choices=status_choices, default='available')
+
     image = models.ImageField(upload_to='books/', blank=True, null=True)
+
+
 
     def __str__(self):
         return self.title
@@ -40,8 +51,11 @@ class Book(models.Model):
 class Transaction(models.Model):
     transaction_id = models.AutoField(primary_key=True)
     date = models.DateField()
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity= models.IntegerField()
+    price = models.IntegerField()
     seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sales')
     buyer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='purchases')
-    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
-
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='orders')
+    
+    payment_method = models.CharField(max_length=255)
+    
--- Backend/bookbounty/users/urls.py
+++ Backend/bookbounty/users/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import RegisterView,LoginView,UserView,LogoutView,BookListAPIView
+from .views import RegisterView,LoginView,UserView,LogoutView,BookListAPIView,OrderAPIView,UserOrdersAPIView,ListingsAPIView
 
 urlpatterns = [
     path('register',RegisterView.as_view()),
@@ -8,4 +8,8 @@ urlpatterns = [
     path('user',UserView.as_view()),
     path('logout',LogoutView.as_view()),
     path('books/', BookListAPIView.as_view(), name='book-list'),
+    path('books/<int:book_id>/', BookListAPIView.as_view(), name='book_api'),
+    path('makeorder/', OrderAPIView.as_view(), name='make-order'),
+    path('userorders/', UserOrdersAPIView.as_view(), name='user-orders'),
+    path('userlistings/', ListingsAPIView.as_view(), name='listings_api'),
 ]
