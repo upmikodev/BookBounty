d3ee74c486e98099847331aa38388e2ab8947a0e
upmikodev <michaelgraham176@outlook.com>
2024-04-19 15:19:29 +0545

    Updated Auth api

--- Backend/bookbounty/users/models.py
+++ Backend/bookbounty/users/models.py
@@ -5,12 +5,14 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-
+    location = models.CharField(max_length=255, null=True, blank=True)
     REQUIRED_FIELDS = ['name']
 
     def __str__(self):
         return self.email
 
+
+    
 class Book(models.Model):
     book_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
@@ -28,9 +30,9 @@ class Book(models.Model):
     rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
     ISBN = models.CharField(max_length=13, null=True, blank=True)
     publication_date = models.DateField(null=True, blank=True)
-    location = models.CharField(max_length=255, null=True, blank=True)
-    category_id = models.IntegerField(null=True, blank=True)
+    category = models.CharField(max_length=255, null=True, blank=True)
     seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='books')
+    image = models.ImageField(upload_to='books/', blank=True, null=True)
 
     def __str__(self):
         return self.title
@@ -41,10 +43,5 @@ class Transaction(models.Model):
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sales')
     buyer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='purchases')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
 
-class Category(models.Model):
-    category_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
