b93a51705ed866efeed317ed7202212b30b23be8
upmikodev <michaelgraham176@outlook.com>
2024-04-20 22:39:52 +0545

    Books checkout done

--- frontend/lib.ts
+++ frontend/lib.ts
@@ -16,3 +16,11 @@ export function getCSRFToken() {
 export function convertImageUrl(url: string): string {
   return url.replace('/http%3A/', 'https://')
 }
+
+export function getTodaysDate() {
+  const today = new Date()
+  const year = today.getFullYear()
+  const month = String(today.getMonth() + 1).padStart(2, '0') // Months are zero-indexed
+  const day = String(today.getDate()).padStart(2, '0')
+  return `${year}-${month}-${day}`
+}
